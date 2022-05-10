9adbe21085a5203ffffb7feb7d6860aad048aea9
Nickwasused <nickwasused.services@protonmail.com>
2022-05-10 16:15:07 +0200

    fix service worker

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const webpack = require('webpack')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
